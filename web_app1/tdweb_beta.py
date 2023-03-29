62a309c37f239df9ffec283a0416b95afc759592
JohlT <126289080+JohlT@users.noreply.github.com>
2023-03-29 00:18:53 -0400

    updated layout of web app

--- web_app1/tdweb_beta.py
+++ web_app1/tdweb_beta.py
@@ -3,19 +3,16 @@ import functions
 
 To_Do = functions.get_To_Do()
 
-st.set_page_config(layout="wide")
-
 def add_todo():
     to_do = st.session_state['new_todo'] + "\n"
     To_Do.append(to_do)
     functions.store_To_Do(To_Do)
 
 
+
 st.title("My Todo App")
 st.subheader("Staying Organized and Consistant")
-st.write("<b>This app is to increase your productivity</b>", unsafe_allow_html=True)
-
-st.text_input(label="", placeholder="Add new item", on_change=add_todo, key='new_todo')
+st.write("This app is to increase your productivity")
 
 for index, todo in enumerate(To_Do):
     checkbox = st.checkbox(todo, key=todo)
@@ -26,5 +23,5 @@ for index, todo in enumerate(To_Do):
         st._rerun()
     
 
-
+st.text_input(label="", placeholder="Add new item", on_change=add_todo, key='new_todo')
 
